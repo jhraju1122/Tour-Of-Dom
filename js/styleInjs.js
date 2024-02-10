03ec65726e9e4526183caceace58daa52dae90d9
jhraju <jhraju890@gmail.com>
2024-02-10 22:48:22 +0600

    added js dom

---
+++ js/styleInjs.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll('section');
+// console.log(sections);
+for(const section of sections){
+    section.style.border = '2px solid red';
+    section.style.marginBottom = '5px' ;
+    section.style.borderRadius = '15px' ;
+    section.style.backgroundColor = 'lightgrey' ;
+}
+
+//specific id dhore style..
+const placeContainer = document.getElementById('places-container');
+placeContainer.style.backgroundColor = 'yellow';
